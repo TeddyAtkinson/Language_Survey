84c9033bc7557b23eaad060ae6c2f171e48e558a
Teddy Atkinson <tedddy.atkinson@gmail.com>
2022-10-21 16:22:16 -0700

    Finalize all files

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
 window.addEventListener("load", function() {
   //console.log("load event")
+  document.getElementById("results").setAttribute("class", "hidden");
   const survey = document.getElementById("survey");
   survey.addEventListener("submit", result);
 });
 
 //------------------------------------------------------------------------------
+
 function result(event) {
   event.preventDefault();
-  //console.log("survey function")
+  //console.log("survey submited")
 
   const selected = document.querySelector("input[name ='q5']:checked").value;
   document.getElementById("results").setAttribute("class", "hidden");
 
   if (selected === "q5a") {
-    document.getElementById("info").innerText = "C++\n\n"
+    document.getElementById("info").innerText = "C++:\n\n"
     + "C++ is a challenging, but extremely benificial language to learn in the"
     + "beginning if you are able to stick with it. C++ allows you to start "
     + "building your knowlege from the ground up and give you a deeper "
@@ -22,7 +24,7 @@ function result(event) {
     document.getElementById("results").setAttribute("class", "pformat");
   }
   else if (selected === "q5b") {
-    document.getElementById("info").innerText = "Python\n\n"
+    document.getElementById("info").innerText = "Python:\n\n"
     + "Python is a good grounds to start learning programing. "
     + "Python is a higher-level programming language, "
     + "meaning it's far from a machine language, making it easier for beginners "
@@ -32,11 +34,11 @@ function result(event) {
     document.getElementById("results").setAttribute("class", "pformat");
   }
   else if (selected === "q5c") {
-    document.getElementById("info").innerText = "JavaScript\n\n"
+    document.getElementById("info").innerText = "JavaScript:\n\n"
     + "JavaScript is very forgiving when it comes to syntax and learning "
     + "the fundamentals. It is very prevalent in the industry making it "
     + "easier to fing a job once you start building your "
-    + "understanding and portfolio"
+    + "understanding and portfolio."
     document.getElementById("results").removeAttribute("class");
     document.getElementById("results").setAttribute("class", "pformat");
   }
