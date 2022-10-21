532601509b22da0e8fa005d22a7870a149b71863
Teddy Atkinson <tedddy.atkinson@gmail.com>
2022-10-21 15:23:48 -0700

    FIXED: working survey and description of survey results

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function() {
-  console.log("load event")
+  //console.log("load event")
   const survey = document.getElementById("survey");
   survey.addEventListener("submit", result);
 });
@@ -7,18 +7,23 @@ window.addEventListener("load", function() {
 //------------------------------------------------------------------------------------------------------------------------
 function result(event) {
   event.preventDefault();
-  console.log("survey function")
-  // const option1 = document.querySelector("input#q5a").value;
-  // const option2 = document.querySelector("input#q5").value;
+  //console.log("survey function")
+
   const selected = document.querySelector("input[name ='q5']:checked").value;
+  document.getElementById("results").setAttribute("class", "hidden");
 
   if (selected === "q5a") {
-    document.getElementById("info").innerText = "C++" + "\nC++ is a challenging, but extremely benificial language to learn in the beggining if you are able to stick with it."
+    document.getElementById("info").innerText = "C++\n\n"
+    + "C++ is a challenging, but extremely benificial language to learn in the beggining if you are able to stick with it."
+    document.getElementById("results").removeAttribute("class");
   }
   else if (selected === "q5b") {
     document.getElementById("info").innerText = "Python" + "\nPython is a good grounds to start learning programing."
+    document.getElementById("results").removeAttribute("class");
   }
   else if (selected === "q5c") {
     document.getElementById("info").innerText = "JavaScript" + "\nJavaScript is very forgiving when it comes to syntax and learning the fundamentals."
+    document.getElementById("results").removeAttribute("class");
   }
+  //console.log(selected);
 }
